37fec8cfe58733e6b0ee358cd160ffc7cb2189d6
YaelDahari <yaeldahari661@gmail.com>
2024-02-08 15:02:31 +0200

    Added the option to capture an image from camera

--- app/src/main/java/com/example/facebook_like_android/utils/ImageHandler.java
+++ app/src/main/java/com/example/facebook_like_android/utils/ImageHandler.java
@@ -5,9 +5,11 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -23,15 +25,27 @@ public class ImageHandler {
         Intent galleryIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
         galleryIntent.addCategory(Intent.CATEGORY_OPENABLE);
         galleryIntent.setType("image/*");
-        activity.startActivityForResult(galleryIntent, REQUEST_CODE_PICK_IMAGE);
-        //activity.startActivity(galleryIntent);
+
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        Intent chooser = Intent.createChooser(galleryIntent, "Select Image");
+        chooser.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] { cameraIntent });
+
+        activity.startActivityForResult(chooser, REQUEST_CODE_PICK_IMAGE);
     }
 
     public Uri handleActivityResult(int requestCode, int resultCode, Intent data, ImageView imageView) {
         Uri photoUri = null;
         if (requestCode == REQUEST_CODE_PICK_IMAGE) {
             if (resultCode == Activity.RESULT_OK && data != null) {
-                photoUri = data.getData();
+                if (data.getData() != null) // Image selected from Gallery
+                    photoUri = data.getData();
+                else if (data.getExtras() != null && data.getExtras().get("data") != null) {
+                    // Image captured from the camera
+                    Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                    imageView.setImageBitmap(bitmap);
+                    photoUri = getImageUri(activity, bitmap);
+                }
                 try {
                     Bitmap bitmap = loadBitmapFromUri(photoUri);
                     imageView.setImageBitmap(bitmap);
@@ -52,6 +66,15 @@ public class ImageHandler {
             options.inSampleSize = 2;
             return BitmapFactory.decodeStream(input, null, options);
         }
+    }
+
+    private Uri getImageUri(Activity activity, Bitmap bitmap) {
+        if (bitmap == null)
+            return null;
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(activity.getContentResolver(), bitmap, "Title", null);
+        return Uri.parse(path);
     }
 }
 
